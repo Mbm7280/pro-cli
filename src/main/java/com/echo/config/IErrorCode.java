a70584b9822040ce1fa775002572cd1b6b58f1c8
Mbm7280 <56158949+Mbm7280@users.noreply.github.com>
2023-10-14 20:29:23 +0800

    Create IErrorCode.java
    统一返回结果类

---
+++ src/main/java/com/echo/config/IErrorCode.java
@@ -0,0 +1,19 @@
+package com.echo.config;
+
+/****************************************************
+ * 创建人：Echo
+ * 创建时间: 2023/10/14 19:54
+ * 项目名称: {cli}
+ * 文件名称: IErrorCode
+ * 文件描述: [API错误接口]
+ * version：1.0
+ * All rights Reserved, Designed By Echo
+ *
+ ********************************************************/
+public interface IErrorCode {
+
+    long getCode();
+
+    String getMessage();
+
+}
