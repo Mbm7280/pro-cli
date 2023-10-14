485a9725a4b9c4afed6627366b87293f1b7a2134
Mbm7280 <56158949+Mbm7280@users.noreply.github.com>
2023-10-14 20:29:30 +0800

    Create ResultCode.java
    统一返回结果类

---
+++ src/main/java/com/echo/config/ResultCode.java
@@ -0,0 +1,39 @@
+package com.echo.config;
+
+public enum ResultCode implements IErrorCode {
+
+    SUCCESS(200, "操作成功"),
+
+    FAILED(500, "操作失败"),
+
+    VALIDATE_FAILED(404, "参数检验失败"),
+
+    UNAUTHORIZED(401, "暂未登录或token已经过期"),
+
+    FORBIDDEN(403, "没有相关权限"),
+
+    PLEASE_APPORTION_PERMISSIONS(405, "请先分配权限"),
+
+    RETURN_VALUE_IS_NULL(990, "返回值为空，请稍后重试"),
+
+
+    ;
+
+    private long code;
+    private String message;
+
+    private ResultCode(long code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+
+    @Override
+    public long getCode() {
+        return code;
+    }
+
+    @Override
+    public String getMessage() {
+        return message;
+    }
+}
